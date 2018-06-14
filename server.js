a7ae178b2e7d467f078855c2db4daf39c4a2d1a7
brianclauzel <brianthomasclauzel@gmail.com>
2018-06-13 23:01:47 -0700

    working on scraping

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var cheerio = require("cheerio");
+var request = require("request");
 var db = require("./models");
 var PORT = 3000;
 
@@ -20,11 +21,42 @@ mongoose.connect(MONGODB_URI);
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/', function(req, res) {
-    res.render('home');
+// app.get('/', function(req, res) {
+    
+// });
+
+app.get("/", function(req, res) {
+    // res.render('home');
+    request("https://www.cnn.com/", function(error, response, body) {
+        // console.log(response);
+        // console.log(body);
+    // if (error) {
+    //     console.log(error);
+    // }
+        
+        var $ = cheerio.load(body);
+        
+
+        $("article h2").each(function(i, element) {
+            console.log(i);
+            console.log(element);
+            var result = {};
+
+            result.title = $(this)
+            .children("a")
+            .text();
+            result.link = $(this)
+            .children("a")
+            .attr("href");
+            console.log(result.title);
+            console.log(result.link);
+        });
+        // console.log(result);
+    });
+    
 });
 
 // app listening on port 300
 app.listen(PORT, function(){
     console.log("App running on port " + PORT);
-})
+});
